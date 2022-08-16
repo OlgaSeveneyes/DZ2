e77569d17371c1b3aa5aa1161a9565af795bf038
Olga <osa_007@mail.ru>
2022-08-16 18:26:19 +0300

    added task2 for dz2

---
+++ Task02/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите число: ");
+string? num = Console.ReadLine();
+int numI;
+
+if (int.TryParse(num, out numI))
+{
+  if (numI<=99)
+  {
+    Console.Write("Третьей цифры нет"); 
+  } 
+  if (numI>99 && numI<1000)
+  {
+    numI = numI % 10;
+    Console.WriteLine(numI);
+  }
+  while (numI>999)
+  {
+    numI = numI / 10;
+    if (numI>99 && numI<1000)
+    {
+      numI = numI % 10;
+      Console.WriteLine(numI);
+    }
+  }
+}
+else
+{
+    Console.WriteLine("Ошибка ввода! Необходимо ввести число");
+}
