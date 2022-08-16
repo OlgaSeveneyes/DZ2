cb63f45e95795570c12dea46ac5b961c41da7de4
Olga <osa_007@mail.ru>
2022-08-16 18:30:51 +0300

    added task3 for dz2

---
+++ Task03/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+string? num = Console.ReadLine();
+int numI;
+
+if (int.TryParse(num, out numI))
+{
+    if (numI>0 && numI<=5)
+    {   
+        Console.WriteLine("Нет");  
+    } 
+    else if (numI>5 && numI<=7)
+    {
+       Console.WriteLine("Да"); 
+    }
+    else if (numI>7)
+    {
+        Console.WriteLine("Ошибка ввода! Необходимо ввести число от 1 до 7");
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка ввода! Необходимо ввести число от 1 до 7");
+} 
