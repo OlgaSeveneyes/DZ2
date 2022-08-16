cb2df36f0a50c16e67cb87b0e05cc1eebaad04bb
Olga <osa_007@mail.ru>
2022-08-16 18:13:20 +0300

    added task1 for dz2

---
+++ Task01/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите трехзначное число: ");
+string? num = Console.ReadLine();
+int numI;
+
+if (int.TryParse(num, out numI))
+{
+    if (numI <= 99)
+    {
+        Console.WriteLine("Ошибка ввода! Введите трехзначное число: ");  
+    }
+    else if (numI > 999)
+    {
+        Console.WriteLine("Ошибка ввода! Введите трехзначное число: ");
+    }
+    else
+    {
+        numI = numI / 10;
+        numI = numI % 10;    
+        Console.WriteLine(numI); 
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка ввода! Необходимо ввести число");
+} 
